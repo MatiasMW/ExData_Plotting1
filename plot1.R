788f9868895e27bdd990db9e943834af68b942c6
MatiasMW <matutemw@gmail.com>
2015-02-08 05:12:30 -0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+household <- read.table("../household_power_consumption.txt",
+                   header=TRUE, sep=";", na.strings="?", dec=".", strip.white=TRUE)
+
+
+
+household1=household
+household1$Date=as.Date(household1$Date, format="%d/%m/%Y")
+
+subhousehold1=household1[household1$Date=="2007-02-01",]
+subhousehold2=household1[household1$Date=="2007-02-02",]
+subhousehold=rbind(subhousehold1,subhousehold2)
+
+hist(subhousehold$Global_active_power, col="orange", xlab="Global Active Power
+     (kilowatts)", ylab="Frequency", main= "Global Active Power")
+
+dev.print(png,filename="../plot1.png",
+          width=480, height=480, pointsize=12, units="px")
+          
+          
